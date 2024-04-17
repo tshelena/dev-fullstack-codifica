64bc67e7998e8d0d9b83bf90b2dc4de1babadf38
tshelena <54648687+tshelena@users.noreply.github.com>
2024-04-17 10:06:01 -0300

    Update convertegraus.js

--- modulo1/convertegraus.js
+++ modulo1/convertegraus.js
@@ -1,13 +1,17 @@
 const prompt = require('prompt-sync')();
 
-let temperaturaCelsius = parseInt(prompt("Insira a temperatura em graus Celsius: "));
-let escala = prompt("Insira a temperatura em graus Celsius: ");
+let temperaturaCelsius = parseFloat(prompt("Insira a temperatura em graus Celsius: "));
 
-let temperaturaConvertida;
+let escala = parseInt(prompt("Insira a escala que deseja converter a temperatura inserida: 1 - Fahrenheit ou 2 - Kelvin  "));
+
+let temperaturaConvertida = 0;
 
 switch (escala) {
-    case 'Fahrenheit':
-        temperaturaConvertida = (temperaturaCelsius * 1,8)+32;
+    case 1:
+        temperaturaConvertida = (temperaturaCelsius * 1.8) + 32;
+        break;
+    case 2:
+        temperaturaConvertida = temperaturaCelsius + 237;
         break;
 }
 
