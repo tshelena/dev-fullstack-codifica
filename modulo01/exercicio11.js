ad616bae060f587f496fe65badf0e002105b5c42
tshelena <54648687+tshelena@users.noreply.github.com>
2024-05-02 10:47:09 -0300

    Exercício 11

--- modulo01/exercicio11.js
+++ modulo01/exercicio11.js
@@ -1,3 +1,10 @@
+/*
+11. Escreva um algoritmo para ler uma quantidade indeterminada de valores inteiros. Para
+cada valor fornecido escrever uma mensagem que indica se cada valor fornecido é PAR
+ou ÍMPAR. O algoritmo será encerrado imediatamente após a leitura de um valor NULO ou
+NEGATIVO.
+*/
+
 const prompt = require('prompt-sync')();
 
 let num;
