6dfaca2079fb8adfb5a0b6b32cd60dd251620726
tshelena <54648687+tshelena@users.noreply.github.com>
2024-05-02 10:25:25 -0300

    Exercício 03

--- modulo01/exercicio03.js
+++ modulo01/exercicio03.js
@@ -1,3 +1,11 @@
+/*
+3. Faça um algoritmo que leia quatro números inteiros e realize as seguintes operações:
+● Some 25 ao primeiro inteiro;
+● Triplique o valor do segundo inteiro;
+● Modifique o valor do terceiro inteiro para 12% do valor original;
+● Armazene no quarto inteiro a soma dos valores originais (os que o usuário digitou)
+dos primeiros três inteiros.
+*/
 const prompt = require('prompt-sync')();
 
 let num1 = parseInt(prompt("Insira o primeiro número: "));
