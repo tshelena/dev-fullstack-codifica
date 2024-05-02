875d7d2ad84411c6b3395b3679a6ea7ff5c5ee74
tshelena <54648687+tshelena@users.noreply.github.com>
2024-05-02 10:09:36 -0300

    Exercício 02

--- modulo01/percentualeleitores.js
+++ modulo01/percentualeleitores.js
@@ -1,3 +1,9 @@
+/*
+2. Escreva um algoritmo para ler o número de eleitores de um município, o número de
+votos brancos, nulos e válidos. Calcular e escrever o percentual que cada um representa
+em relação ao total de eleitores.
+*/
+
 const prompt = require('prompt-sync')();
 
 let total = parseInt(prompt("Insira a quantidade total de eleitores: "));
