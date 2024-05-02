55a2500c0e5710865580f4a4b4bbd705cdd82a2b
tshelena <54648687+tshelena@users.noreply.github.com>
2024-05-02 10:44:37 -0300

    Exercício 09

--- modulo01/exercicio09.js
+++ modulo01/exercicio09.js
@@ -1,3 +1,12 @@
+/*
+9. Escreva um algoritmo que leia o código de origem de um produto e imprima a região
+do mesmo, conforme a tabela abaixo:
+1 - Sul                     5 ou 6 - Nordeste
+2 - Norte                   7, 8 ou 9 - Sudeste
+3 - Leste                   10 até 20 - Centro - Oeste
+4 - Oeste                   25 até 50 - Nordeste
+*/
+
 const prompt = require('prompt-sync')();
 
 let codOrigem = Number(prompt("Digite qual a origem do produto: "));
