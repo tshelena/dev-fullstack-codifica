164a41f1195ab58e9e774c4b5f36b84183f9d976
tshelena <54648687+tshelena@users.noreply.github.com>
2024-05-02 10:45:25 -0300

    Exercício 10

--- modulo01/exercicio10.js
+++ modulo01/exercicio10.js
@@ -1,3 +1,7 @@
+/*
+10. Escreva um algoritmo para ler um número inteiro e escrevê-lo na tela 10 vezes.
+*/
+
 const prompt = require('prompt-sync')();
 
 let num = parseInt(prompt("Insira um número inteiro: "));
