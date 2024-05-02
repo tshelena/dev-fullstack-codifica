ea270c6a2c34e3686d6169fae248faa6fc2e3535
tshelena <54648687+tshelena@users.noreply.github.com>
2024-05-02 10:04:13 -0300

    Exercício 01

--- modulo01/convertegraus.js
+++ modulo01/convertegraus.js
@@ -1,18 +1,12 @@
+/*
+1. Escreva um algoritmo para ler uma temperatura em graus Celsius, 
+calcular e escrever o valor correspondente em graus Fahrenheit.
+*/
+
 const prompt = require('prompt-sync')();
 
 let temperaturaCelsius = parseFloat(prompt("Insira a temperatura em graus Celsius: "));
 
-let escala = parseInt(prompt("Insira a escala que deseja converter a temperatura inserida: 1 - Fahrenheit ou 2 - Kelvin  "));
-
-let temperaturaConvertida = 0;
-
-switch (escala) {
-    case 1:
-        temperaturaConvertida = (temperaturaCelsius * 1.8) + 32;
-        break;
-    case 2:
-        temperaturaConvertida = temperaturaCelsius + 237;
-        break;
-}
+let temperaturaConvertida = (temperaturaCelsius * 1.8) + 32;
 
 console.log(temperaturaConvertida)
